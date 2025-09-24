c948492597f02e37aa036424f933ef7af4c563f3
Linnia Hawkins <linnia@crhtc65.hpc.ucar.edu>
2025-09-24 12:34:43 -0600

    add scripts to setup basecases

---
+++ gen_ensembles/jobscripts/step0.startup.sh
@@ -0,0 +1,5 @@
+#check out codebase
+cd /glade/work/linnia/
+git clone --branch ctsm5.3.065 https://github.com/ESCOMP/CTSM.git ctsm5.3.065
+cd ctsm5.3.065
+./bin/git-fleximod update
--- gen_ensembles/jobscripts/step1.setup_basecases.sh
+++ gen_ensembles/jobscripts/step1.setup_basecases.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
-# create base cases for ctsm5.2.027 
+# create base cases for ctsm5.3.065 
 
 # ===============================================
 # setup
 
-tag_dir="/glade/work/linnia/ctsm5.3.0"
-chargenum='P93300641'
+tag_dir="/glade/work/linnia/ctsm5.3.065/"
+chargenum='P93300041'
 
 source ~/.bashrc
 
-USER_MODS_DIR='/glade/u/home/linnia/CLM6-PPE/run_clm/jobscripts/user_mods'
+USER_MODS_DIR='/glade/u/home/linnia/CLM6-PPE/gen_ensembles/jobscripts/user_mods'
 LND_MESH_FILE=${USER_MODS_DIR}/lnd_mesh.nc
 
-paramfile='/glade/work/linnia/ctsm5.3.0/cime/scripts/transient/runtime_files/ctsm60_params.c241007.nc'
+paramfile='/glade/work/linnia/CLM6-PPE/ctsm6_cal/paramfiles/cal115_c08132025.nc'
 
-# AD restart file
-finidat='/glade/campaign/cgd/tss/people/oleson/CLM5_restarts/ctsm530_f19_PPE_pSASU.clm2.r.0161-01-01-00000.nc'
+# restart file for AD
+finidat='/glade/work/linnia/ctsm5.3.065/cime/scripts/transient/runtime_files/ctsm5.3.065_transient_postSASU_cal115.clm2.r.0121-01-01-00000.nc'
 
 # ==============================================
 # Build and run basecases
 conda activate runclm
 
 cd ${tag_dir}/cime/scripts/
-casedir="/glade/work/linnia/ctsm5.3.0/cime/scripts/transient/basecases/"
-case="ctsm5.3.0_transient"
+casedir=${tag_dir}/cime/scripts/transient/basecases/
+
+case="ctsm5.3.065_transient"
 
 # do these one at a time 
 do_AD=0
 do_SASU=0
 do_postSASU=0
-do_transient=1
+do_transient=0
+do_ssp=1
 
 # ==============================================
 # Setup and run AD spinup
@@ -60,7 +62,7 @@ echo "finidat = '$finidat'" >> user_nl_clm
 # env_run.xml
 ./xmlchange RUN_STARTDATE=0001-01-01
 ./xmlchange STOP_OPTION=nyears
-./xmlchange STOP_N=20
+./xmlchange STOP_N=80
 ./xmlchange MASK_MESH=${LND_MESH_FILE}
 ./xmlchange ATM_DOMAIN_MESH=${LND_MESH_FILE}
 ./xmlchange LND_DOMAIN_MESH=${LND_MESH_FILE}
@@ -92,7 +94,7 @@ echo "finidat = '$finidat'" >> user_nl_clm
 ./xmlchange ROOTPE_CPL=16
 
 # env_workflow.xml
-./xmlchange JOB_WALLCLOCK_TIME=03:00:00
+./xmlchange JOB_WALLCLOCK_TIME=09:00:00
 ./xmlchange JOB_PRIORITY=regular
 
 # change paramfile
@@ -108,7 +110,7 @@ echo "paramfile = '$paramfile'" >> user_nl_clm
 ./case.build
 
 # Submit case
-./case.submit
+#./case.submit
 
 fi
 
@@ -135,14 +137,11 @@ cd ${casedir}${case}${suffix}
 cp ${USER_MODS_DIR}/user_nl_datm_streams_CRUJRA user_nl_datm_streams
 cp ${USER_MODS_DIR}/user_nl_clm_SASU user_nl_clm
 
-finidat=`ls -1 ${SCRATCH}/${case}_AD/run/${case}_AD.clm?.r.*.nc | tail -1`
-echo $finidat
-echo "finidat = '$finidat'" >> user_nl_clm
-
 # env_run.xml
 ./xmlchange RUN_STARTDATE=0001-01-01
 ./xmlchange STOP_OPTION=nyears
-./xmlchange STOP_N=80
+./xmlchange STOP_N=120
+./xmlchange RESUBMIT=2
 ./xmlchange MASK_MESH=${LND_MESH_FILE}
 ./xmlchange ATM_DOMAIN_MESH=${LND_MESH_FILE}
 ./xmlchange LND_DOMAIN_MESH=${LND_MESH_FILE}
@@ -174,7 +173,7 @@ echo "finidat = '$finidat'" >> user_nl_clm
 ./xmlchange ROOTPE_CPL=16
 
 #env_workflow.xml
-./xmlchange JOB_WALLCLOCK_TIME=06:00:00
+./xmlchange JOB_WALLCLOCK_TIME=10:00:00
 ./xmlchange JOB_PRIORITY=regular
 
 # change paramfile
@@ -188,7 +187,7 @@ echo "paramfile = '$paramfile'" >> user_nl_clm
 ./case.build
 
 # Submit case
-./case.submit
+#./case.submit
 
 fi
 
@@ -215,14 +214,10 @@ cd ${casedir}${case}${suffix}
 cp ${USER_MODS_DIR}/user_nl_datm_streams_CRUJRA user_nl_datm_streams
 cp ${USER_MODS_DIR}/user_nl_clm_postSASU user_nl_clm
 
-finidat=`ls -1 ${SCRATCH}/${case}_SASU/run/${case}_SASU.clm?.r.*.nc | tail -1`
-echo $finidat
-echo "finidat = '$finidat'" >> user_nl_clm
-
 # env_run.xml
 ./xmlchange RUN_STARTDATE=0001-01-01
 ./xmlchange STOP_OPTION=nyears
-./xmlchange STOP_N=40
+./xmlchange STOP_N=120
 ./xmlchange MASK_MESH=${LND_MESH_FILE}
 ./xmlchange ATM_DOMAIN_MESH=${LND_MESH_FILE}
 ./xmlchange LND_DOMAIN_MESH=${LND_MESH_FILE}
@@ -254,7 +249,7 @@ echo "finidat = '$finidat'" >> user_nl_clm
 ./xmlchange ROOTPE_CPL=16
 
 #env_workflow.xml
-./xmlchange JOB_WALLCLOCK_TIME=03:00:00
+./xmlchange JOB_WALLCLOCK_TIME=11:00:00
 ./xmlchange JOB_PRIORITY=regular
 
 # change paramfile
@@ -268,10 +263,9 @@ echo "paramfile = '$paramfile'" >> user_nl_clm
 ./case.build
 
 # Submit case
-./case.submit
+#./case.submit
 
 fi
-
 
 # ==============================================
 # Setup Transient (don't run)
@@ -293,15 +287,10 @@ cd ${tag_dir}/cime/scripts/
 
 cd ${casedir}${case}
 
-cp ${USER_MODS_DIR}/user_nl_datm_streams_CRUJRA user_nl_datm_streams
-cp ${USER_MODS_DIR}/user_nl_datm_streams_CRUJRA.2015-2023 .
+cp ${USER_MODS_DIR}/user_nl_datm_streams_1850 user_nl_datm_streams
 cp ${USER_MODS_DIR}/user_nl_clm_transient_1850 ./user_nl_clm
 cp ${USER_MODS_DIR}/user_nl_clm_daily_1985 .
-cp ${USER_MODS_DIR}/user_nl_clm_3hourly_2001 .
-
-finidat=`ls -1 ${SCRATCH}/${case}_postSASU/run/${case}_postSASU.clm?.r.*.nc | tail -1`
-echo $finidat
-echo "finidat = '$finidat'" >> user_nl_clm
+cp ${USER_MODS_DIR}/user_nl_clm_ssp_landuse_2015 .
 
 # env_run.xml
 ./xmlchange MASK_MESH=${LND_MESH_FILE}
@@ -338,7 +327,7 @@ echo "finidat = '$finidat'" >> user_nl_clm
 ./xmlchange ROOTPE_CPL=16
 
 #env_workflow.xml
-./xmlchange JOB_WALLCLOCK_TIME=10:00:00
+./xmlchange JOB_WALLCLOCK_TIME=08:00:00
 ./xmlchange JOB_PRIORITY=regular
 
 # change paramfile
@@ -355,6 +344,80 @@ echo "paramfile = '$paramfile'" >> user_nl_clm
 # At this point, run_ens.sh script should be run with a config file.
 # clm5ppe/jobscripts/run_ens.sh
 # ./run_ens.sh *.config >& tmp.out &
+
+fi
+
+# ==============================================
+# Setup SSP
+# ==============================================
+
+if [ "$do_ssp" -eq 1 ]; then
+
+# create new case
+suffix="_SSP370"
+
+if [ -d "${casedir}${case}${suffix}" ]; then
+  rm -r -f "${casedir}${case}${suffix}"
+fi
+if [ -d "${SCRATCH}${case}${suffix}" ]; then
+  rm -r -f "${SCRATCH}${case}${suffix}"
+fi
+
+./create_newcase --case ${casedir}${case}${suffix} --compset SSP370_DATM%CRUJRA2024_CLM60%BGC_SICE_SOCN_SROF_SGLC_SWAV_SESP --res f19_g17 --project $chargenum --run-unsupported
+
+cd ${casedir}${case}${suffix}
+
+cp ${USER_MODS_DIR}/user_nl_clm_ssp370 ./user_nl_clm
+cp ${USER_MODS_DIR}/user_nl_datm_streams_CRUJRA_ssp370 ./user_nl_datm_streams
+
+# env_run.xml
+./xmlchange MASK_MESH=${LND_MESH_FILE}
+./xmlchange ATM_DOMAIN_MESH=${LND_MESH_FILE}
+./xmlchange LND_DOMAIN_MESH=${LND_MESH_FILE}
+./xmlchange RUN_STARTDATE=2024-01-01
+./xmlchange DATM_YR_ALIGN=2024
+./xmlchange DATM_YR_START=2005
+./xmlchange DATM_YR_END=2014
+./xmlchange CLM_BLDNML_OPTS='-bgc bgc -no-megan'
+./xmlchange DOUT_S=FALSE
+./xmlchange CLM_ACCELERATED_SPINUP=off
+./xmlchange STOP_OPTION=nyears
+./xmlchange STOP_N=77
+
+# env_mach_pes.xml
+./xmlchange PIO_STRIDE_ATM=4
+
+./xmlchange NTASKS_ATM=16
+./xmlchange NTASKS_OCN=112
+./xmlchange NTASKS_WAV=112
+./xmlchange NTASKS_GLC=112
+./xmlchange NTASKS_ICE=112
+./xmlchange NTASKS_ROF=112
+./xmlchange NTASKS_LND=112
+./xmlchange NTASKS_CPL=112
+
+./xmlchange ROOTPE_ATM=0
+./xmlchange ROOTPE_LND=16
+./xmlchange ROOTPE_OCN=16
+./xmlchange ROOTPE_WAV=16
+./xmlchange ROOTPE_GLC=16
+./xmlchange ROOTPE_ICE=16
+./xmlchange ROOTPE_ROF=16
+./xmlchange ROOTPE_CPL=16
+
+#env_workflow.xml
+./xmlchange JOB_WALLCLOCK_TIME=12:00:00
+./xmlchange JOB_PRIORITY=economy
+
+# change paramfile
+echo "paramfile = '$paramfile'" >> user_nl_clm
+
+./case.setup
+
+./preview_namelists
+
+#qcmd -A ${chargenum} -- 
+./case.build
 
 fi
 
